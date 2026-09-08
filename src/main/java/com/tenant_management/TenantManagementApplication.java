b48d0eccf25d1110f8b5ac8c35bdf16afb5c2c10
Vyshnavi Annabathina <vyshnaviannabathina@gmail.com>
2026-09-08 12:02:31 +0530

    Initial Tenant Management Sprint 2 setup

---
+++ src/main/java/com/tenant_management/TenantManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tenant_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenant_management/TenantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenant_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
