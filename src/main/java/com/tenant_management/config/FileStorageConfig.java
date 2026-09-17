7935f01837223e73b374a2adda4a5f145df0f822
Gaurav <ankurraj2209@gmail.com>
2026-09-17 13:44:26 +0530

    made directory change for uploads folder

--- src/main/java/com/tenant_management/config/FileStorageConfig.java
+++ src/main/java/com/tenant_management/config/FileStorageConfig.java
@@ -9,6 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
   public class FileStorageConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**") .addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/uploads/**") .addResourceLocations("file:platform-administration-service/uploads/");
     }
   }
